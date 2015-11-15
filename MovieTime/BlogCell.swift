427d1e45bfadc162ddd66ec1013922dc6dc371cf
RitaHsu <la75312312@gmail.com>
2015-11-15 12:16:01 +0800

    add bloggers view

---
+++ MovieTime/BlogCell.swift
@@ -0,0 +1,16 @@
+//
+//  BlogCell.swift
+//  MovieTime
+//
+//  Created by Ko LiChung on 11/14/15.
+//  Copyright © 2015 Ko LiChung. All rights reserved.
+//
+
+import UIKit
+
+class BlogCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+}
